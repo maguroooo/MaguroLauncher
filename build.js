c86ed6313f94f1f560f08acf07fb3c700710f32c
まーぐーろ <maguroshouta@gmail.com>
2021-01-20 10:47:19 +0900

    Update build.js

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'magurolauncher',
+        productName: 'MaguroLauncher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2020 Daniel Scalzi, 2021 maguroooo',
         directories: {
             buildResources: 'build',
             output: 'dist'
